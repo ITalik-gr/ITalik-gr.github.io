bb8df5f8ea01bcfbd6d308c7f522176d7164995e
v.ITalik <vitalik.gricenko77@gmail.com>
2020-03-14 16:27:19 +0200

    All pages are ready without adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,29 @@ var box = document.getElementById('menu');
 toggle.addEventListener('click', function() {
   box.classList.toggle('active');
 });
+
+//! Scroll 
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+//! Slider 
+
+$('.slider').slick({
+    prevArrow: '<button id="prev" type="button" class="prev"> &#8249; New Post</button>',
+    nextArrow: '<button id="next" type="button" class="next">Old Post &#8250;</button>',
+  
+});
+  
